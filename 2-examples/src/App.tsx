f2d9d84156fc349cabbb302841e1268010300429
vanbd71 <vannguyen02092003@gmail.com>
2023-01-04 20:57:46 +0700

    a

---
+++ 2-examples/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import images from './assets/images/abc.webp'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Name: <h4 className='text-info'>Nguyễn Đức Vân</h4></p>
+        <p>From: <h4 className='text-info'>Quảng Nam</h4></p>
+        <p>Phone Number: <h4 className='text-info'>0932511249</h4></p>
+        {/* <img src="{images}" alt="" /> */}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ex/b1.js
@@ -0,0 +1,7 @@
+var h=5;
+
+for(let i=1; i<h; i++) {
+    for(let j=1; j<i;j++) {
+        console.log("*");
+    }
+}
